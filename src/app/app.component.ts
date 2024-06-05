dc58240a4d7d5cedfe9717713bfe424e13d9f0db
Titusrono <tituskrono04@gmail.com>
2024-06-05 08:14:14 +0300

    enviroment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule,NavbarComponent,FooterComponent,RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ServicesComponent } from './services/services.component';
+import { ContactComponent } from './contact/contact.component';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
+import { AboutComponent } from './about/about.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {'path':'','title':'Home', component:HomeComponent},
+    {'path':'services','title':'Service',component:ServicesComponent},
+    {'path':'contact', 'title':'Contact',component:ContactComponent},
+    {'path':'about', 'title':'About',component:AboutComponent},
+    {'path':'**','title':'404',component:PagenotfoundComponent}
+];
